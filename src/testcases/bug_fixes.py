3c706a7951bd630403fc6a4732484d6613323f1f
johantenbroeke <johan@fullscrteen.nl>
2021-12-14 15:40:28 +0100

    added testcase for all data quality bugs after ACC testing

--- src/testcases/bug_fixes.py
+++ src/testcases/bug_fixes.py
@@ -10,6 +10,10 @@ from kjk.test_utils import (
     ErkenningsnummerNotFoudError,
 )
 
+# These tests all expose data quality bugs
+# The bugs occurred while testing allocations on ACC data
+# NOTE: There are no assertions, tests succeed if no exeptions are raised
+
 
 class EviCrashBugTestCase(unittest.TestCase):
     def setUp(self):
@@ -20,9 +24,72 @@ class EviCrashBugTestCase(unittest.TestCase):
         market_allocation = self.allocator.get_allocation()
 
 
-class BaranchesCrashBugTestCase(unittest.TestCase):
+class BranchesCrashBugTestCase(unittest.TestCase):
+    def setUp(self):
+        dp = FixtureDataprovider("../fixtures/branches-bug.json")
+        self.allocator = Allocator(dp)
+
+    def test_bug(self):
+        market_allocation = self.allocator.get_allocation()
+
+
+class BranchesCrashBug2TestCase(unittest.TestCase):
     def setUp(self):
         dp = FixtureDataprovider("../fixtures/branches-bug2.json")
+        self.allocator = Allocator(dp)
+
+    def test_bug(self):
+        market_allocation = self.allocator.get_allocation()
+
+
+class AllocationBugA12TestCase(unittest.TestCase):
+    def setUp(self):
+        dp = FixtureDataprovider("../fixtures/12A-allocation-bug.json")
+        self.allocator = Allocator(dp)
+
+    def test_bug(self):
+        market_allocation = self.allocator.get_allocation()
+
+
+class MaximumPlacesBugTestCase(unittest.TestCase):
+    def setUp(self):
+        dp = FixtureDataprovider("../fixtures/maximum-plaatsen-bug.json")
+        self.allocator = Allocator(dp)
+
+    def test_bug(self):
+        market_allocation = self.allocator.get_allocation()
+
+
+class MaximumPlacesBug2TestCase(unittest.TestCase):
+    def setUp(self):
+        dp = FixtureDataprovider("../fixtures/maximum-plaatsen-bug2.json")
+        self.allocator = Allocator(dp)
+
+    def test_bug(self):
+        market_allocation = self.allocator.get_allocation()
+
+
+class Phase00BugTestCase(unittest.TestCase):
+    def setUp(self):
+        dp = FixtureDataprovider("../fixtures/phase_00-bug.json")
+        self.allocator = Allocator(dp)
+
+    def test_bug(self):
+        market_allocation = self.allocator.get_allocation()
+
+
+class ValueErrorBugTestCase(unittest.TestCase):
+    def setUp(self):
+        dp = FixtureDataprovider("../fixtures/value-error-bug.json")
+        self.allocator = Allocator(dp)
+
+    def test_bug(self):
+        market_allocation = self.allocator.get_allocation()
+
+
+class VplBugTestCase(unittest.TestCase):
+    def setUp(self):
+        dp = FixtureDataprovider("../fixtures/vpl-bug.json")
         self.allocator = Allocator(dp)
 
     def test_bug(self):
