36c66a808fefd8bdbf0e21d86760b108251f9e72
johantenbroeke <johan@fullscrteen.nl>
2022-01-25 23:14:37 +0100

    added more diagrams

---
+++ diagrams/render.sh
@@ -0,0 +1,9 @@
+docker run -u 1000 UID -it -v /home/johan/Projects/amsterdam/origin/makkelijkemarkt-allocations/diagrams:/data minlag/mermaid-cli -i /data/src/overview.mmd -o /data/img/overview.svg
+docker run -u 1000  -it -v /home/johan/Projects/amsterdam/origin/makkelijkemarkt-allocations/diagrams:/data minlag/mermaid-cli -i /data/src/fase_1.mmd -o /data/img/fase_1.svg
+docker run -u 1000  -it -v /home/johan/Projects/amsterdam/origin/makkelijkemarkt-allocations/diagrams:/data minlag/mermaid-cli -i /data/src/fase_2.mmd -o /data/img/fase_2.svg
+docker run -u 1000  -it -v /home/johan/Projects/amsterdam/origin/makkelijkemarkt-allocations/diagrams:/data minlag/mermaid-cli -i /data/src/fase_3.mmd -o /data/img/fase_3.svg
+docker run -u 1000  -it -v /home/johan/Projects/amsterdam/origin/makkelijkemarkt-allocations/diagrams:/data minlag/mermaid-cli -i /data/src/fase_3-b.mmd -o /data/img/fase_3-b.svg
+docker run -u 1000  -it -v /home/johan/Projects/amsterdam/origin/makkelijkemarkt-allocations/diagrams:/data minlag/mermaid-cli -i /data/src/fase_4.mmd -o /data/img/fase_4.svg
+docker run -u 1000  -it -v /home/johan/Projects/amsterdam/origin/makkelijkemarkt-allocations/diagrams:/data minlag/mermaid-cli -i /data/src/fase_5.mmd -o /data/img/fase_5.svg
+docker run -u 1000  -it -v /home/johan/Projects/amsterdam/origin/makkelijkemarkt-allocations/diagrams:/data minlag/mermaid-cli -i /data/src/fase_6.mmd -o /data/img/fase_6.svg
+docker run -u 1000  -it -v /home/johan/Projects/amsterdam/origin/makkelijkemarkt-allocations/diagrams:/data minlag/mermaid-cli -i /data/src/fase_7.mmd -o /data/img/fase_7.svg
