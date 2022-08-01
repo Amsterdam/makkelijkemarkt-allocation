cc38ed35b3ffc2d2e3a57fe053494916193c8701
Harm Manders <h.manders@amsterdam.nl>
2022-08-01 14:32:35 +0200

    Updated diagrams for changes made in may

--- diagrams/render.sh
+++ diagrams/render.sh
@@ -2,17 +2,17 @@
 
 diagrams=( 
     overview 
-    fase_1 
-    fase_2-3-4 
-    fase_5
-    fase_6-7-8-9-10-11
-    fase_12-13-14-15-16
-    fase_17-18-19-20
-    fase_21-22-23-24
-    fase_25
-    fase_26
-    vinden_kramen
-    vinden_uitbreiding
+    # fase_1 
+    # fase_2-3-4 
+    # fase_5
+    # fase_6-7-8-9-10-11
+    # fase_12-13-14-15-16
+    # fase_17-18-19-20
+    # fase_21-22-23-24
+    # fase_25
+    # fase_26
+    # vinden_kramen
+    # vinden_uitbreiding
 )
 
 filetypes=(
@@ -25,7 +25,7 @@ do
 
 for i in "${diagrams[@]}"
 do
-    docker run -u 1000  -it -v /home/johan/Projects/amsterdam/origin/makkelijkemarkt-allocations/diagrams:/data minlag/mermaid-cli -i /data/src/$i.mmd -o /data/img/$i.$f
+    docker run -u 1000  -it -v /home/harm/Documents/amsterdam/repositories/makkelijkemarkt/makkelijkemarkt-allocation/diagrams:/data minlag/mermaid-cli -i /data/src/$i.mmd -o /data/img/$i.$f
 done
 
 done
