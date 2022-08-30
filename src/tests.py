d987c10f811e60276355cd75c00c76b486a7c685
Harm Manders <h.manders@amsterdam.nl>
2022-08-30 15:42:14 +0200

    added test case for expanding soll no flex

--- src/tests.py
+++ src/tests.py
@@ -25,6 +25,7 @@ from testcases.crash import *
 from testcases.bug_50364_soll_does_not_get_2_stands import *
 from testcases.bug_Markt133 import *
 from testcases.vpl_expansion_prio import *
+from testcases.soll_expand_nonFlex import *
 
 # unit-test
 from test_allocation import *
