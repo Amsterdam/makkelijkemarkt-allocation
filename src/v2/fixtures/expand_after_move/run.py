15d70fc19b3a518a21602cb66b5066a7de4b042e
Pim-020 <97027864+Pim-020@users.noreply.github.com>
2023-01-31 16:21:21 +0100

    Add expand_after_move run file

---
+++ src/v2/fixtures/expand_after_move/run.py
@@ -0,0 +1,18 @@
+import json
+
+from v2.allocate import allocate
+from v2.conf import logger, KraamTypes, trace
+from v2.fixtures.expand_after_move.kramen import rows
+from v2.fixtures.expand_after_move.branches import branches
+from v2.fixtures.expand_after_move.ondernemers import ondernemers
+from v2.fixtures.expand_after_move.markt import markt_meta
+
+
+if __name__ == '__main__':
+    logger.local = True
+    output = allocate(markt_meta, rows, branches, ondernemers)
+    logs = logger.get_logs()
+    json.dumps(logs)
+    print(logs)
+
+    # trace.show()
