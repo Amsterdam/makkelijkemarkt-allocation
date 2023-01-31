65196064047c1ff58f2d93b6c7a054087e5389b3
Pim-020 <97027864+Pim-020@users.noreply.github.com>
2023-01-31 16:48:21 +0100

    Update fixtures expand_after_move

--- src/v2/fixtures/expand_after_move/kramen.py
+++ src/v2/fixtures/expand_after_move/kramen.py
@@ -1,47 +1,16 @@
 from v2.kramen import Kraam
-from v2.fixtures.branches import branches
+from v2.fixtures.expand_after_move.branches import branches
 
 branche_1, branche_2, branche_3, branche_4 = branches
 
 rows = [
     [
-        Kraam(id=1, ondernemer=None, branche=branche_1),
-        Kraam(id=2, ondernemer=None, branche=branche_1),
+        Kraam(id=1, ondernemer=None, branche=None),
+        Kraam(id=2, ondernemer=None, branche=None),
         Kraam(id=3, ondernemer=None, branche=None),
-        Kraam(id=4, ondernemer=None, branche=branche_2),
-        Kraam(id=5, ondernemer=None, branche=branche_2),
-        Kraam(id=6, ondernemer=None, branche=branche_2),
     ],
     [
         Kraam(id=7, ondernemer=None, branche=None),
         Kraam(id=8, ondernemer=None, branche=None),
-        Kraam(id=9, ondernemer=None, branche=None),
-        Kraam(id=10, ondernemer=None, branche=None, bak=True),
-        Kraam(id=11, ondernemer=None, branche=branche_3),
-        Kraam(id=12, ondernemer=None, branche=branche_4),
-        Kraam(id=13, ondernemer=None, branche=branche_4),
-    ],
-    [
-        Kraam(id=14, ondernemer=None, branche=None, bak=True, evi=True),
-        Kraam(id=15, ondernemer=None, branche=None, bak=True, evi=True),
-    ],
-    [
-        Kraam(id=16, ondernemer=None, branche=branche_1),
-        Kraam(id=17, ondernemer=None, branche=None),
-        Kraam(id=18, ondernemer=None, branche=None),
-        Kraam(id=19, ondernemer=None, branche=None),
-    ],
-    [
-        Kraam(id=20, ondernemer=None, branche=branche_1),
-        Kraam(id=21, ondernemer=None, branche=branche_1),
-        Kraam(id=22, ondernemer=None, branche=branche_1),
-        Kraam(id=23, ondernemer=None, branche=branche_1),
-        Kraam(id=24, ondernemer=None, branche=branche_1, bak=True),
-        Kraam(id=25, ondernemer=None, branche=branche_1),
-        Kraam(id=26, ondernemer=None, branche=None, bak_licht=True),
-        Kraam(id=27, ondernemer=None, branche=None, bak_licht=True),
-        Kraam(id=28, ondernemer=None, branche=None, evi=True),
-        Kraam(id=29, ondernemer=None, branche=None, evi=True),
-        Kraam(id=30, ondernemer=None, branche=branche_3, bak=True),
     ],
 ]
--- src/v2/fixtures/expand_after_move/ondernemers.py
+++ src/v2/fixtures/expand_after_move/ondernemers.py
@@ -1,46 +1,22 @@
 from v2.ondernemers import Ondernemer
 from v2.conf import Status
-from v2.fixtures.branches import branches
+from v2.fixtures.expand_after_move.branches import branches
 
 branche_1, branche_2, branche_3, branche_4 = branches
 
 vpls = [
-    Ondernemer(rank=1, branche=branche_1, prefs=[16, 4, 5], min=1, max=3, anywhere=True, status=Status.VPL, own=[6]),
-    Ondernemer(rank=2, branche=branche_2, prefs=[7, 3, 5], min=1, max=3, anywhere=False, status=Status.VPL, own=[5]),
-    Ondernemer(rank=3, branche=branche_3, prefs=[10, 11], min=1, max=2, anywhere=False, status=Status.VPL, own=[3, 4]),
-    Ondernemer(rank=4, branche=branche_1, prefs=[12, 18, 19], min=1, max=4, anywhere=False, status=Status.TVPL, own=[24]),
-    Ondernemer(rank=5, branche=branche_2, prefs=[20, 9], min=1, max=3, anywhere=True, status=Status.TVPLZ, own=['tvplz1']),
-    Ondernemer(rank=6, branche=branche_1, prefs=[20, 21, 22], min=1, max=2, anywhere=True, status=Status.TVPL, own=[20]),
+    Ondernemer(rank=1, branche=None, prefs=[7, 8], min=1, max=3, anywhere=True, status=Status.VPL, own=[1, 2]),
+    Ondernemer(rank=2, branche=None, prefs=[], min=1, max=3, anywhere=False, status=Status.VPL, own=[3]),
 ]
 
 ebs = [
-    Ondernemer(rank=21, branche=branche_1, prefs=[21, 22, 23], min=1, max=3, anywhere=False, status=Status.EB, own=[22]),
-    Ondernemer(rank=22, branche=branche_2, prefs=[27, 28], min=1, max=3, anywhere=False, status=Status.EB, own=[27]),
 ]
 
 sollicitanten = [
-    Ondernemer(rank=101, branche=branche_1, prefs=[1, 4, 2, 3], min=1, max=3, anywhere=False, status=Status.SOLL),
-    Ondernemer(rank=102, branche=branche_1, prefs=[20, 3], min=1, max=3, anywhere=True, status=Status.SOLL),
-    Ondernemer(rank=120, branche=branche_2, prefs=[5, 6], min=1, max=1, anywhere=True, status=Status.SOLL),
-    Ondernemer(rank=130, prefs=[6, 7], min=1, max=2, anywhere=True, status=Status.SOLL),
-    Ondernemer(rank=140, branche=branche_4, prefs=[8, 9], min=2, max=4, anywhere=False, status=Status.SOLL,
-               bak_licht=True),
-    Ondernemer(rank=150, branche=branche_4, prefs=[13, 20], min=1, max=4, anywhere=True, status=Status.SOLL,
-               bak=True, evi=True),
-    Ondernemer(rank=160, branche=branche_4, prefs=[1, 2], min=1, max=4, anywhere=True, status=Status.SOLL,
-               bak_licht=True, evi=True),
-    Ondernemer(rank=170, branche=branche_4, prefs=[3, 4], min=1, max=4, anywhere=True, status=Status.SOLL,
-               evi=True),
+    Ondernemer(rank=101, branche=None, prefs=[1], min=1, max=3, anywhere=False, status=Status.SOLL),
 ]
 
 b_lijst = [
-    Ondernemer(rank=201, branche=branche_1, prefs=[4], min=1, max=3, anywhere=True, status=Status.B_LIST),
-    Ondernemer(rank=202, branche=branche_1, prefs=[4], min=1, max=3, anywhere=True, status=Status.B_LIST),
-    Ondernemer(rank=220, branche=branche_2, prefs=[10, 18], min=1, max=1, anywhere=True, status=Status.B_LIST),
-    Ondernemer(rank=230, branche=branche_3, prefs=[17], min=1, max=2, anywhere=True, status=Status.B_LIST,
-               bak=True),
-    Ondernemer(rank=240, branche=branche_4, prefs=[20], min=1, max=1, anywhere=True, status=Status.B_LIST,
-               bak_licht=True),
 ]
 
 ondernemers = [*vpls, *ebs, *sollicitanten, *b_lijst]
