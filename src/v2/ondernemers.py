f8f0a2fa4921e401f7dd33e774edc41c025c88d5
Pim-020 <97027864+Pim-020@users.noreply.github.com>
2023-02-07 16:25:40 +0100

    Add ignored prop to ondernemer

--- src/v2/ondernemers.py
+++ src/v2/ondernemers.py
@@ -23,6 +23,7 @@ class Ondernemer(TraceMixin):
         self.kraam_type = KraamType(bak, bak_licht, evi)
         self.is_rejected = False
         self.reject_reason = ''
+        self.ignored = False
 
     def __str__(self):
         return f"Ondernemer {self.rank}, {self.status.value}, b={self.branche.shortname}, min={self.min}, max={self.max}, " \
@@ -43,6 +44,9 @@ class Ondernemer(TraceMixin):
     def unassign_kraam(self, kraam):
         self.branche.assigned_count -= 1
         self.kramen.remove(kraam)
+
+    def ignore(self):
+        self.ignored = True
 
     def reject(self, reason):
         self.trace.log(f"Rejecting: {reason.value} => {self}")
@@ -95,7 +99,7 @@ class Ondernemers:
         filters = []
         selected = []
         prop_names = ['status', 'branche', 'kraam_type']
-        bool_prop_names = ['anywhere', 'is_soft_rejected']
+        bool_prop_names = ['anywhere', 'is_soft_rejected', 'ignored']
         for kwarg in filter_kwargs:
             if kwarg in [*prop_names, *bool_prop_names]:
                 filters.append(lambda ondernemer, kwarg=kwarg: getattr(ondernemer, kwarg) == filter_kwargs[kwarg])
